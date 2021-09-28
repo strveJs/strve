7d4c6e1f0c4a696fc2032563ccdb41772d35a227
maomincoding <17864296568@163.com>
2021-09-28 16:18:12 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import state from './state.js'
-import mountNode from './diff.js'
-
-function createView(v) {
-    state._data = v.data;
-    state._template = v.template;
-    state._el = v.el;
-    v.el ? mountNode(v.template, v.el) : console.error("Error: Please set el property!");
-}
-
+export { createView } from './createView.js'
 export { template } from './vnode.js'
 export { eventListener } from './eventListener.js'
 export { reactive, ref } from './reactivety.js'
-export { createView }
