5b08ffd78964209fc29e23f225d3b1d614d2512d
maomincoding <17864296568@163.com>
2023-07-01 00:48:04 +0800

    v5.5.0

--- dist/strve.full.js
+++ dist/strve.full.js
@@ -1,5 +1,5 @@
 /*!
- * Strve.js v5.2.3
+ * Strve.js v5.5.0
  * (c) 2021-2023 maomincoding
  * Released under the MIT License.
  */
@@ -582,7 +582,7 @@
         }
     }
 
-    const version = '5.2.3';
+    const version = '5.5.0';
     const state = {
         _el: null,
         _template: null,
--- dist/strve.runtime-esm.js
+++ dist/strve.runtime-esm.js
@@ -1,5 +1,5 @@
 /*!
- * Strve.js v5.2.3
+ * Strve.js v5.5.0
  * (c) 2021-2023 maomincoding
  * Released under the MIT License.
  */
@@ -576,7 +576,7 @@ function defineCustomElement(options, tag) {
     }
 }
 
-const version = '5.2.3';
+const version = '5.5.0';
 const state = {
     _el: null,
     _template: null,
